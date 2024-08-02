b8965d3f6952b3ef744aec69950d1aa364589392
RafaelDuke <rafaelfuriads@hotmail.com>
2024-08-01 22:08:39 -0300

    Aula 4

---
+++ dio.trilha.java.basico/src/dio/trilha/java/basico/TesteAula4.java
@@ -0,0 +1,21 @@
+package dio.trilha.java.basico;
+
+public class TesteAula4 {
+
+	public static void main(String[] args) {
+		// Variáveis e Constantes
+		
+		int numero = 2;
+		numero = 10;
+		System.out.println(numero);
+		
+		//Ao tentar alterar o Valor da Constante Dara erro.
+		final double VALOR_DE_PI = 3.14;
+		//Ex VALOR_DE_PI = 10.75;
+		System.out.println(VALOR_DE_PI);
+		
+		
+
+	}
+
+}
