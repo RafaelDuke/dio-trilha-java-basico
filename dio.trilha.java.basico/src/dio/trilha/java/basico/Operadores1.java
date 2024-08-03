5b8b722d65e6230a378b2576cb4dac0a41babfd0
RafaelDuke <rafaelfuriads@hotmail.com>
2024-08-03 00:01:51 -0300

    Aula Operadores

---
+++ dio.trilha.java.basico/src/dio/trilha/java/basico/Operadores1.java
@@ -0,0 +1,38 @@
+package dio.trilha.java.basico;
+
+public class Operadores1 {
+
+	public static void main(String[] args) {
+		
+		String nomeCompleto = "linguagem" + "java";
+		String concatenacao = "?";
+		
+		System.out.println(nomeCompleto);
+		System.out.println("");
+		
+		concatenacao = 1 + 1 + 1 + "1";
+		System.out.println(concatenacao);
+		System.out.println("");
+		
+		concatenacao = 1 + "1" + 1 + 1;
+		System.out.println(concatenacao);
+		System.out.println("");
+		
+		concatenacao = 1 + "1" + 1 + "1";
+		System.out.println(concatenacao);
+		System.out.println("");
+		
+		concatenacao = "1" + 1 + 1 + 1;
+		System.out.println(concatenacao);
+		System.out.println("");
+		
+		concatenacao = "1" +( 1 + 1 + 1);
+		System.out.println(concatenacao);
+		System.out.println("");
+		
+		
+		
+
+	}
+
+}
---
+++ dio.trilha.java.basico/src/dio/trilha/java/basico/operador2.java
@@ -0,0 +1,21 @@
+package dio.trilha.java.basico;
+
+public class operador2 {
+
+	public static void main(String[] args) {
+		int numero = 5;
+		
+		System.out.println(-numero);
+		System.out.println("");
+		
+		System.out.println(numero);
+		System.out.println("");
+		
+		numero = -numero;
+		System.out.println(-numero);
+		
+		numero = numero * -1;
+		System.out.println(numero);
+	}
+
+}
