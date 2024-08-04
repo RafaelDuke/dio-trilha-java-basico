5ec21ae2b7b2f9ca42a95c1da43fc02ec43ccd59
RafaelDuke <rafaelfuriads@hotmail.com>
2024-08-04 17:06:06 -0300

    Aula 7 Operadoes lógicos

---
+++ dio.trilha.java.basico/src/dio/trilha/java/basico/OperadoresLogicos7.java
@@ -0,0 +1,25 @@
+package dio.trilha.java.basico;
+
+public class OperadoresLogicos7 {
+
+	public static void main(String[] args) {
+		
+		//Tabela verdade
+		
+		boolean condicao1=true;
+		boolean condicao2=false;
+		
+		if(condicao1 && condicao2) {
+			System.out.println("As duas condições são verdadeiras");
+			
+		}
+		
+		if(condicao1 || condicao2) {
+			System.out.println("Uma Das condições são Verdadeiras");
+			
+		}
+		
+
+	}
+
+}
